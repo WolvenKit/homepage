5f4b405ba8ae8de0f923505f3ecea3cf1cd0e2aa
Zhincore <adam@zhincore.eu>
2024-07-13 18:54:29 +0200

    Started to make multipage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,16 +8,22 @@
   config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
   import sammy from "$assets/sammy.png";
+  import Button from "$lib/parts/Button.svelte";
 </script>
 
-<header class="sticky top-0 z-40 flex w-full items-center justify-between px-8 py-2">
+<header class="sticky top-0 z-40 flex w-full items-center justify-between bg-zinc-900/50 px-8 py-2 backdrop-blur">
   <a href="/" class="flex flex-wrap items-center gap-8">
     <img src={sammy} width="128" height="128" alt="" class="h-24 w-auto" />
 
     <h1 class="text-4xl font-bold uppercase leading-none text-red md:text-5xl lg:text-6xl">Red Modding</h1>
   </a>
 
-  <nav>a</nav>
+  <nav>
+    <ul class="flex gap-2">
+      <li><Button href="/projects">Projects</Button></li>
+      <li><Button href="/about">About us</Button></li>
+    </ul>
+  </nav>
 </header>
 
 <slot />
