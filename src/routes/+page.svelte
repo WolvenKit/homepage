adec2e710dd45ee917b5958db7d04adc1071e279
Adam Žingor <adam@zhincore.eu>
2024-07-23 23:54:40 +0200

    Now is it muted

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,10 @@
   import Card from "$lib/parts/Card.svelte";
   import Section from "$lib/parts/Section.svelte";
 
+  let video: HTMLVideoElement;
   let bgLoaded = false;
+
+  $: if (video) video.defaultMuted = true;
 </script>
 
 <header class="relative flex min-h-[calc(100svh-7rem)] flex-col">
@@ -26,11 +29,14 @@
   >
     <div class="dots absolute inset-0" />
     <video
+      bind:this={video}
       use:mediaReady={() => (bgLoaded = true)}
       class="h-full w-full bg-black mix-blend-multiply transition-all hover:mix-blend-luminosity"
       autoplay
       loop
-      muted={true}
+      playsinline
+      muted
+      defaultMuted
       disablepictureinpicture
       disableremoteplayback
       preload=""
@@ -39,7 +45,6 @@
       <source src={malorianHEVC} type="video/mp4; codecs=hevc" />
       <source src={malorianAV1} type="video/webm; codecs=av1" />
       <source src={malorianMP4} type="video/mp4; codecs=avc1.640028" />
-      <Image src={malorian} />
     </video>
   </div>
 
